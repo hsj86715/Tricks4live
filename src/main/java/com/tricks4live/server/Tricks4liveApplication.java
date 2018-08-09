803a169e9f32951f427512675c5617eb1038b786
hsj86715 <hsj86715@gmail.com>
2018-08-09 20:31:37 +0800

    Initial commit

---
+++ src/main/java/com/tricks4live/server/Tricks4liveApplication.java
@@ -0,0 +1,12 @@
+package com.tricks4live.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tricks4liveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tricks4liveApplication.class, args);
+	}
+}
---
+++ src/main/java/com/tricks4live/server/controller/LoginController.java
@@ -0,0 +1,9 @@
+package com.tricks4live.server.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+}
